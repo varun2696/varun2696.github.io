9ec10e2768c7e45c18d4917942f7dd2db68d3d14
varun <varuvrk02@gmail.com>
2023-09-15 19:51:40 +0530

    about section updated

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -9,21 +9,19 @@ export const About = () => {
       <h1 className="flex justify-center items-center text-center p-[5%] text-4xl md:text-5xl text-gray-300 font-bold capitalize">
         About Me
       </h1>
-      <p className="text-xl font-bodyFont pb-[5%]">
+      <p className="text-xl font-bodyFont pb-[5%] leading-9">
         Hello, I am <span className="text-designColor">Varun KR</span> from{" "}
-        <span className="text-designColor">Karnataka</span>. I have completed my
+        <span className="text-designColor">Karnataka.</span> I have completed my
         graduation in Bachelor of Science (PCM) from Davanagere University.
       </p>
       <p
         id="user-detail-intro"
-        className="text-xl font-bodyFont leading-6 tracking-wide"
+        className="text-xl font-bodyFont leading-9 tracking-wide"
       >
-        A passionate coder who has studied Full Stack Web Development at{" "}
-        <span className="text-designColor">Masai School</span> and gained over
-        1000 hours of coding experience. With my knowledge and skills, I have
-        built more than 10 websites. My proficiency in the{" "}
-        <span className="text-designColor">MERN</span> stack and my passion for
-        coding make me a valuable asset in any development team.
+        "As a passionate coder, I've undergone comprehensive Full Stack Web Development training at
+        <span className="p-2 text-designColor"> Masai School , </span>
+        accumulating over 1000 hours of hands-on coding experience. During my journey, I successfully developed and launched more than 10 websites. Proficient in the
+        <span className="text-designColor"> MERN</span> stack (MongoDB, Express.js, React.js, Node.js), my dedication to coding makes me a valuable asset to any development team."
       </p>
     </div>
   );
