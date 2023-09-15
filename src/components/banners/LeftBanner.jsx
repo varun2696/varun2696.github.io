be41c30e385dcf071fdc4bec3008df3c3eb20a48
varun <varuvrk02@gmail.com>
2023-09-15 16:24:13 +0530

    changes

--- src/components/banners/LeftBanner.jsx
+++ src/components/banners/LeftBanner.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { useTypewriter, Cursor } from "react-simple-typewriter";
-import { FaFacebookF, FaTwitter, FaLinkedinIn, FaReact } from "react-icons/fa";
-import { SiTailwindcss, SiFigma, SiNextdotjs } from "react-icons/si";
+import { FaLinkedinIn, FaGithub, FaDownload } from "react-icons/fa";
 
 const LeftBanner = () => {
+  
   const [text] = useTypewriter({
     words: ["Passionate Coder.", "Full Stack Developer."],
     loop: true,
@@ -11,6 +11,8 @@ const LeftBanner = () => {
     deleteSpeed: 10,
     delaySpeed: 2000,
   });
+
+
   return (
     <div className="w-full lgl:w-1/2 flex flex-col gap-20">
       <div className="flex flex-col gap-5">
@@ -29,11 +31,8 @@ const LeftBanner = () => {
             cursorColor="#ff014f"
           />
         </h2>
-        {/* <p id="user-detail-intro" className="text-base font-bodyFont leading-6 tracking-wide">
-          A passionate coder who has studied MERN at Masai School and gained over 1000 hours of coding experience. With my knowledge and skills, I have built more than 10 websites. My proficiency in the MERN stack and my passion for coding make me a valuable asset in any development team.
-        </p> */}
       </div>
-      <div className="flex flex-col xl:flex-row gap-6 lgl:gap-0 justify-between">
+      <div className="flex flex-row justify-between">
         <div>
           <h2 className="text-base uppercase font-titleFont mb-4">
             Find me in
@@ -44,6 +43,36 @@ const LeftBanner = () => {
                 <FaLinkedinIn />
               </a>
             </span>
+            <span className="bannerIcon">
+              <a
+                href="https://github.com/varun2696"
+                target="_blank"
+              >
+                <FaGithub />
+              </a>
+            </span>
+          </div>
+        </div>
+
+        <div>
+          <div className='resumeLi'>
+            <div>
+              <FaDownload />
+            </div>
+            <a
+              href="/Varun-KR-Resume.pdf"
+              download="Varun-KR-Resume.pdf"
+              id="resume-link-2"
+              onClick={() => {
+                window.open(
+                  "https://drive.google.com/file/d/1kZ12BmKJCJWiRh3YbND2zncih26N4j9h/view?usp=sharing"
+                );
+              }}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Resume
+            </a>
           </div>
         </div>
       </div>
