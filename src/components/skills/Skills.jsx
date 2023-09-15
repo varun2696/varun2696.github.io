089c1a9850ff6668c481f01d6978cdff415e83c2
varun <varuvrk02@gmail.com>
2023-09-15 16:28:54 +0530

    seperated skills and tools

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -13,6 +13,7 @@ import {
   SiNodedotjs,
   SiNpm,
   SiPostman,
+  SiReact,
   SiReactrouter,
   SiRedux,
   SiRender,
@@ -21,19 +22,17 @@ import {
   SiVisualstudiocode,
 } from "react-icons/si";
 
-import { FaReact } from "react-icons/fa";
-
 const Skills = () => {
   return (
     <div id="skills" className="w-full py-20 border-b-[1px] border-b-black">
       <h1 className="flex justify-center items-center text-center p-[5%] text-4xl md:text-5xl text-gray-300 font-bold capitalize">
-        My Skills
+        Skills
       </h1>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-8 gap-6 xl:gap-10">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-7 gap-6 xl:gap-10">
         <Card icon={<SiHtml5 size={"50px"} />} name={"HTML5"} />
         <Card icon={<SiCss3 size={"50px"} />} name={"CSS3"} />
         <Card icon={<SiJavascript size={"50px"} />} name={"Javascript"} />
-        <Card icon={<FaReact size={"50px"} />} name={"React"} />
+        <Card icon={<SiReact size={"50px"} />} name={"React"} />
         <Card icon={<SiRedux size={"50px"} />} name={"Redux"} />
         <Card icon={<SiReactrouter size={"50px"} />} name={"Router"} />
         <Card icon={<SiNodedotjs size={"50px"} />} name={"Node.js"} />
@@ -41,6 +40,12 @@ const Skills = () => {
         <Card icon={<SiExpress size={"50px"} />} name={"Express"} />
         <Card icon={<SiChakraui size={"50px"} />} name={"Chakra-Ui"} />
         <Card icon={<SiTailwindcss size={"50px"} />} name={"Tailwind"} />
+      </div>
+
+      <h1 className="flex justify-center items-center text-center p-[5%] text-4xl md:text-5xl text-gray-300 font-bold capitalize">
+        Tools
+      </h1>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-7 gap-6 xl:gap-10">
         <Card icon={<SiGithub size={"50px"} />} name={"GitHub"} />
         <Card icon={<SiPostman size={"50px"} />} name={"Postman"} />
         <Card icon={<SiNpm size={"50px"} />} name={"NPM"} />
