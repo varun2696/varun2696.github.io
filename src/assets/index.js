57aa57528c80dd99cf72cec236ca88dfcb5b7c8c
varun <varuvrk02@gmail.com>
2023-09-16 17:29:18 +0530

    added youtube project

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,10 +3,12 @@ import bannerImg from "./images/bannerImg.png";
 import projectOne from "./images/projects/projectOne.jpg";
 import projectTwo from "./images/projects/projectTwo.jpg";
 import projectThree from "./images/projects/projectThree.jpeg";
+import projectFour from "./images/projects/projectFour.jpg";
 
 export {
   bannerImg,
   projectOne,
   projectTwo,
   projectThree,
+  projectFour
 };
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { projectOne, projectThree, projectTwo, } from "../../assets/index";
+import { projectOne, projectThree, projectTwo, projectFour } from "../../assets/index";
 import ProjectsCard from "./ProjectsCard";
 
 const Projects = () => {
@@ -11,7 +11,17 @@ const Projects = () => {
       <h1 className="flex justify-center items-center text-center p-[5%] text-4xl md:text-5xl text-gray-300 font-bold capitalize">
         My Projects
       </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-2 gap-6 xl:gap-16">
+
+        <ProjectsCard
+          title="PharmEasy"
+          des="Clone of an E-commerce website PharmEasy, an online pharmacy and healthcare platform based in India."
+          src={projectTwo}
+          github={"https://github.com/AditiSharma00/decorous-pan-7664"}
+          deployedLink={"https://pharmeasy-tan.vercel.app/"}
+          teckStack={"Javascript | CSS | HTML | React | Redx | Ckakra Ui"}
+        />
+
         <ProjectsCard
           title="Sephora"
           des="An e-commerce website where users can buy beauty
@@ -24,15 +34,6 @@ const Projects = () => {
         />
 
         <ProjectsCard
-          title="PharmEasy"
-          des="Clone of an E-commerce website PharmEasy, an online pharmacy and healthcare platform based in India."
-          src={projectTwo}
-          github={"https://github.com/AditiSharma00/decorous-pan-7664"}
-          deployedLink={"https://pharmeasy-tan.vercel.app/"}
-          teckStack={"Javascript | CSS | HTML | React | Redx | Ckakra Ui"}
-        />
-
-        <ProjectsCard
           title="Apple.com"
           des="Clone of an Apple.com, an online platform for the multinational technology company."
           src={projectThree}
@@ -40,6 +41,16 @@ const Projects = () => {
           deployedLink={"https://matrix-opal.vercel.app/"}
           teckStack={'Javascript | React | Ckakra Ui | Node | Express | MongoDb'}
         />
+
+        <ProjectsCard
+          title="Yotube"
+          des="This is a simple YouTube clone built with React.js and styled using Tailwind CSS. It allows users to browse and watch YouTube videos."
+          src={projectFour}
+          github={"https://github.com/varun2696/youtube-clone"}
+          deployedLink={"https://vkr-yt-react-app.netlify.app/"}
+          teckStack={'Javascript | React | TailwindCss'}
+        />
+
       </div>
     </section>
   );
